99d186b5e06c5d6e3bbe842ff64cd4f282553d0c
sdikyar <92029382+sdikyar@users.noreply.github.com>
2026-07-13 04:25:25 +0300

    first commit

---
+++ CrossDomainKD/configs/retinanet/retinanet_r18_fpn_2x_coco_mini.py
@@ -0,0 +1,24 @@
+_base_ = [
+    '../_base_/models/retinanet_r50_fpn.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+work_dir = './work_dirs/retina_r18_mini'
+# optimizer
+model = dict(
+    backbone=dict(
+        depth=18,
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18')),
+    neck=dict(in_channels=[64, 128, 256, 512]))
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+
+runner = dict(type='EpochBasedRunner', max_epochs=20)
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=2,)
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (2 samples per GPU)
+auto_scale_lr = dict(base_batch_size=16)
